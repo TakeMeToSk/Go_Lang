83444183de4b0f5e7d67b2f54dc64f6c8383c7df
jingbinh <hanabienjoy123@gmail.com>
2022-11-19 22:30:30 +0800

    Little changes

--- test.go
+++ test.go
@@ -1,10 +1,22 @@
 package main
-import "ftm"
 
-func main(){
+import (
+	"fmt"
+)
 
-    fmt.Println("hello Go lang!!!")
-
-
+type Student struct {
+	Name  string
+	Age   int
+	Class string
 }
 
+func main() {
+	var jingbinh Student
+	jingbinh.Name = "jingbinh"
+	jingbinh.Age = 24
+	jingbinh.Class = "LUT CS-N1"
+	momo := Student{"baimenghao", 23, "LUT CS-N1"}
+	fmt.Println(momo.Age)
+	fmt.Println(momo.Class)
+	fmt.Println(momo.Name)
+}
